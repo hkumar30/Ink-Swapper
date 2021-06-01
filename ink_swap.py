bcc6f3cf1ff8b5502545e8de9b4c1868fa2c0fa7
harshmax <dhirharsh8180@gmail.com>
2021-06-01 19:21:42 +0530

    First Commit

---
+++ ink_swap.py
@@ -0,0 +1,80 @@
+"""
+This program changes the colour of the ink, 
+based on input provided by the user.
+"""
+
+from simpleimage import SimpleImage
+
+DEFAULT_FILE = 'images/pen.jpg'
+INTENSITY_THRESHOLD = 1.12
+
+def avg_colour(pixel):
+    """
+    Returns the average of RGB values of that corresponding
+    pixel.
+    """
+    return (pixel.red + pixel.blue + pixel.green) / 3
+
+def ink_swap(image, colour):
+    """
+    This function identifies the "sufficiently blue" pixels i.e.
+    the "Royal Blue" ink and changes its colour as per the input
+    entered by the user.
+    """
+    for pixel in image:
+        if pixel.blue >= avg_colour(pixel) * INTENSITY_THRESHOLD:
+            if colour == 'Red' or colour == 'red':
+                pixel.red, pixel.green, pixel.blue = 172, 50, 53
+            elif colour == 'Black' or colour == 'black':
+                pixel.red, pixel.green, pixel.blue = 68, 65, 60
+            elif colour == 'Green' or colour == 'green':
+                pixel.red, pixel.green, pixel.blue = 17, 136, 123
+            elif colour == 'Blue Black' or colour == 'blue black' or colour == 'Blue black':
+                pixel.red, pixel.green, pixel.blue = 0, 0, 102
+            elif colour == 'Purple' or colour == 'purple':
+                pixel.red, pixel.green, pixel.blue = 154, 44, 160
+            # elif colour == '<add your colour name>'
+            #     pixel.red, pixel.green, pixel.blue = Red, Green, Blue
+
+    return image
+
+def main():
+    print('Welcome to Ink Swapper!')
+    print('')
+    filename = get_file()
+    image = SimpleImage(filename)
+    ink_swap_img = SimpleImage(filename)
+
+    print('Choose a colour from the following options: ')
+    print('a)','Black')
+    print('b)','Red')
+    print('c)','Green')
+    print('d)','Blue Black')
+    print('e)','Purple')
+    colour = str(input('Your Choice: '))
+    if colour != 'Red' and colour != 'red' and colour != 'Black' and colour != 'black' and colour != 'Green' and colour != 'green' and colour != 'Blue Black' and colour != 'blue black' and colour != 'Blue black' and colour != 'Purple' and colour != 'purple':
+        print('Your choice is not present in our database.') 
+        print('Switching to Blue-Black Ink instead.')
+        colour = 'Blue Black'
+
+    print('')
+    print('Please wait...')
+    ink_swap(ink_swap_img, colour)
+
+    image.show()
+    ink_swap_img.show()
+    print('')
+    print('© Harsh Kumar, 2021')
+    print('Exclusively for Code-in-Place, Stanford University, CA 94305')
+
+def get_file():
+    # Read image file path from user, or use the default file
+    print('You can upload your image in the "images" folder and use') 
+    print('/home/images/<name of img>.jpg')
+    filename = input('Enter image file (or press enter for default): ')
+    if filename == '':
+        filename = DEFAULT_FILE
+    return filename
+
+if __name__ == '__main__':
+    main()
